70e4752371afb3386bb31310df12e5f78838e14d
Jason Truluck <jason.truluck@gmail.com>
2013-03-27 17:59:52 -0400

    add load api and specs to cover

---
+++ lib/subordinate/client/load.rb
@@ -0,0 +1,23 @@
+# Load
+module Subordinate
+  class Client
+    # Load statistics
+    #
+    # @see https://ci.jenkins-ci.org/job/jenkins_rc_branch/api/
+    module Load
+      # Returns the current load statistics of the Jenkins server
+      #
+      # @see http://jenkins.missioncontrol.io:8080/queue/api/
+      #
+      # @return [Hashie::Mash] load statistics response
+      #
+      # @example Get the current load statistics
+      #   Subordinate::Client.load_statistics
+      #
+      # @author Jason Truluck
+      def load_statistics(options = {})
+        get("overallLoad/api/json", options)
+      end
+    end
+  end
+end
