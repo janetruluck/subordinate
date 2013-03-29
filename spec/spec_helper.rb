7e7c7d04c20d1011a0ad4d41f2a0759d1c54828d
Jason Truluck <jason.truluck@gmail.com>
2013-03-28 22:13:30 -0400

    Remove File.open
    doh!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,7 @@ require 'vcr'
 require "webmock/rspec"
 require "mocha/api"
 
-authentications = File.open(File.expand_path("../fixtures/authentications.yml", __FILE__))
+authentications = File.expand_path("../fixtures/authentications.yml", __FILE__)
 if File.exists?(authentications)
   ENV.update YAML::load(File.open(authentications))
 end
