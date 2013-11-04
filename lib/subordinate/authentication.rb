40c382a28af34a3363929d6c0f4ece8c2897e236
Jason Truluck <jason.truluck@gmail.com>
2013-11-04 12:09:56 -0500

    Remove a lot of leg work from authentication and only including authenticated? method to confirm existence of username and token for auth.

--- lib/subordinate/authentication.rb
+++ lib/subordinate/authentication.rb
@@ -1,22 +1,8 @@
 # Authentication Module
 module Subordinate
   module Authentication
-    def authentication
-      if username && api_token
-        { :username => username, :api_token => api_token}
-      else
-        {}
-      end
-    end
-
     def authenticated?
-      !authentication.empty?
-    end
-
-    def username_and_token(username = "", api_token = "")
-      return if username.nil? || api_token.nil?
-      self.username   = username
-      self.api_token  = api_token
+      !!(username && api_token)
     end
   end
 end
